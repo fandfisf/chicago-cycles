ce4dd645a994d4df908f063801f45feb1189c1e9
Prashant Khanwale <pskhanwale@gmail.com>
2016-05-15 15:36:03 -0700

    Add ability to actually query local Elastic Search DB

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -9,6 +9,14 @@ import play.api.libs.json.Json
   */
 case class Station(id:Int,stationName:String,availableDocks:Int,totalDocks:Int,latitude:Double,longitude:Double,statusValue:String,statusKey:Int,availableBikes:Int,stAddress1:String,stAddress2:Option[String],city:String,postalCode:Option[String],location:Option[String],altitude:Option[Double],testStation:Boolean,lastCommunicationTime:Option[Date],landMark:Option[String],renting:Boolean,is_renting:Boolean)
 object  Station {
-  val stationWrites = Json.writes[Station]
-  val stationReads = Json.reads[Station]
+  implicit val stationWrites = Json.writes[Station]
+  implicit val stationReads = Json.reads[Station]
+}
+case class Shard (total: Int,successful: Int, failed: Int)
+object Shard {
+  implicit val shardReads = Json.reads[Shard]
+}
+case class Count(count:Int, _shards:Shard)
+object Count {
+  implicit val countReads = Json.reads[Count]
 }
--- app/services/BikeDataReader.scala
+++ app/services/BikeDataReader.scala
@@ -2,26 +2,40 @@ package services
 
 import javax.inject.{Inject, Singleton}
 
+import models.Count
+import models.Count._
 import play.api.libs.ws.WSClient
 import play.api.{Configuration, Logger}
+
+import scala.concurrent.ExecutionContext
+
 /**
   * Created by Prashant S Khanwale @ Suveda LLC  on 5/15/16.
   */
-trait BikeDataReader{
-  def pullData (): Unit
+trait BikeDataReader {
+  def pullData(): Unit
 }
+
 @Singleton
-class BikeDataReaderImpl @Inject()(ws:WSClient, configuration:Configuration) extends BikeDataReader{
-  def pullData (): Unit ={
-      Logger.info(s"Pulling data from ${configuration.getString("elasticSearch.inputDataUrl").getOrElse("URL not specified") }")
-//    configuration.getString("elasticSearch.inputDataUrl") match {
-//      case Some(url) => {
-//        val futureResult: Future[List[Station]] = ws.url(url).get().map {
-//          response =>
-//            (response.json \ "stationBeanList" ).as[List[Station]]
-//        }
-//      }
+class BikeDataReaderImpl @Inject()(ws: WSClient, configuration: Configuration)(implicit exec: ExecutionContext) extends BikeDataReader {
+  def pullData(): Unit = {
+    Logger.info(s"Pulling data from ${configuration.getString("elasticSearch.inputDataUrl").getOrElse("URL not specified")}")
+    configuration.getString("elasticSearch.indexName") match {
+      case Some(index) =>
+        configuration.getString("elasticSearch.baseUrl") match {
+          case Some(url) => {
+            ws.url(url + "/"+index+"/_count").get().map {
+              response =>
+                Logger.info(s"Local elastic search response [${
+                  response.body
+                }]")
+                val c:Count = countReads.reads(response.json).asOpt.get
+                Logger.info(s"Found ${c.count} occurrences")
+            }
+          }
+        }
     }
-
-
+  }
 }
+
+//  (response.json \ "stationBeanList" ).as[List[Station]]
